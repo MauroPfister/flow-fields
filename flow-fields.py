14b995e4f0768835a35c935f19f480c0686165a3
Mauro Pfister <mauro.pfister@epfl.ch>
2021-11-29 00:12:24 +0100

    Plot lines with correct width
    - Define line widths as full width not only half the thickness.
    - Add custom line class to plot lines with correct width.
    - Change variable names

--- flow-fields.py
+++ flow-fields.py
@@ -2,7 +2,34 @@ from time import perf_counter
 import numpy as np
 import numba
 from matplotlib import pyplot as plt
+from matplotlib.lines import Line2D
 from perlin2D import generate_fractal_noise_2d, generate_perlin_noise_2d
+
+class Line(Line2D):
+    """Custom line class.
+    The line width is defined in the same units as the plotted data.
+    E.g. a line of length 1.0 and width 1.0 looks like a square.
+    In contrast, the width of a line plotted with plt.plot() is defined
+    in points.
+    """
+    def __init__(self, *args, **kwargs):
+        _lw_data = kwargs.pop("linewidth", 1) 
+        super().__init__(*args, **kwargs)
+        self._lw_data = _lw_data
+
+    def _get_lw(self):
+        if self.axes is not None:
+            ppd = 72./self.axes.figure.dpi
+            trans = self.axes.transData.transform
+            return ((trans((1, self._lw_data))-trans((0, 0)))*ppd)[1]
+        else:
+            return 1
+
+    def _set_lw(self, lw):
+        self._lw_data = lw
+
+    _linewidth = property(_get_lw, _set_lw)
+
 
 @numba.jit(fastmath=True, cache=True)
 def lerp(a, b, x):
@@ -40,26 +67,26 @@ def get_v_field(v_field, x, y):
            lerp(v_field[:, cell_y, cell_x + 1], v_field[:, cell_y + 1, cell_x + 1], frac_x),
            frac_y)
 
-def is_collision(point, lines, widths, d_sep):
+def is_collision(point, width, lines, widths, safety_fac=1.5):
     if len(lines) < 1:
         return False
 
     # Array with line width of every point on every line 
-    widths_vec = np.hstack([[width]*line.shape[0] for line, width in zip(lines, widths)])
-
+    widths_all_points = np.repeat(widths, [line.shape[0] for line in lines], axis=0)
     all_points = np.vstack(lines)
+    # Squared distance between query point and points of all the other lines
     dist = np.sum((all_points - point)**2, axis=1)
-    mask = dist < ((widths_vec + d_sep)**2) * 1.5
+    is_collision = dist < (((widths_all_points + width) / 2)**2) * safety_fac
 
-    return np.any(mask)
+    return np.any(is_collision)
 
 def out_of_bounds(point, x_lim, y_lim):
     return point[0] < x_lim[0] or point[0] > x_lim[1] or point[1] < y_lim[0] or point[1] > y_lim[1]
 
-def trace_line(seed_point, lines, widths, v_field, step_size, d_sep):
+def trace_line(start_point, width, lines, widths, v_field, step_size):
     x_lim = [0, 1]
     y_lim = [0, 1]
-    line = [seed_point]
+    line = [start_point]
 
     max_length = 0.1
 
@@ -68,7 +95,7 @@ def trace_line(seed_point, lines, widths, v_field, step_size, d_sep):
         point = line[-1]
         vel = get_v_field(v_field, *point)
         point_new = point + vel * step_size
-        if is_collision(point_new, lines, widths, d_sep) or out_of_bounds(point_new, x_lim, y_lim):
+        if is_collision(point_new, width, lines, widths) or out_of_bounds(point_new, x_lim, y_lim):
             break
         line.append(point_new)
 
@@ -77,7 +104,7 @@ def trace_line(seed_point, lines, widths, v_field, step_size, d_sep):
         point = line[-1]
         vel = get_v_field(v_field, *point)
         point_new = point - vel * step_size
-        if is_collision(point_new, lines, widths, d_sep) or out_of_bounds(point_new, x_lim, y_lim):
+        if is_collision(point_new, width, lines, widths) or out_of_bounds(point_new, x_lim, y_lim):
             break
         line.append(point_new)
     
@@ -86,8 +113,8 @@ def trace_line(seed_point, lines, widths, v_field, step_size, d_sep):
 def trace_field(field, step_size):
     lines = []
     widths = []
-    
-    width_scale = 0.08
+
+    width_scale = 0.16
     max_attempts = 1000  # Max attempts at finding a valid starting point
     n_lines_max = 300
     for _ in range(n_lines_max):
@@ -98,15 +125,15 @@ def trace_field(field, step_size):
         width = max(float(scale), step_size / 5)
 
         # Try to generate valid starting point for new line
-        p_start_valid = False
+        start_point_valid = False
         for _ in range(max_attempts):
-            p_start = np.random.rand(2)
-            if not is_collision(p_start, lines, widths, width):
-                p_start_valid = True
+            start_point = np.random.rand(2)
+            if not is_collision(start_point, width, lines, widths):
+                start_point_valid = True
                 break
 
-        if p_start_valid:
-            line = trace_line(p_start, lines, widths, field, step_size, width)
+        if start_point_valid:
+            line = trace_line(start_point, width, lines, widths, field, step_size)
             lines.append(line)
             widths.append(width)
         else:
@@ -130,7 +157,7 @@ color_palettes = {
 
 if __name__ == "__main__":
     step_size = 0.005
-    d_sep = 0.01
+    width = 0.01
 
     angle_field = np.pi * generate_fractal_noise_2d((1000, 1000), (1, 1), octaves=2, persistence=2)
     # angle_field = np.pi * np.round(generate_perlin_noise_2d((1000, 1000), (2, 2)) * 4) / 4
@@ -142,65 +169,46 @@ if __name__ == "__main__":
 
     lines, widths = trace_field(v_field, step_size)
 
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax.axis("equal")
+    fig = plt.figure()
+    fig.set_size_inches(8, 8)
+    ax = plt.Axes(fig, [0, 0, 1, 1])
+    fig.add_axes(ax)
     ax.set_xlim([-0.1, 1.1])
     ax.set_ylim([-0.1, 1.1])
-    ax.set_xticks([])
-    ax.set_yticks([])
-
+    ax.set_axis_off()
 
     plot_style = "lines"
-    outline = False
 
     accent = color_palettes["autumn"]
     selection = color_palettes["autumn"]
     colors = np.random.choice(selection, len(lines))
 
-
-    for line, d_sep, color in zip(lines, widths, colors):
+    for line, width, color in zip(lines, widths, colors):
         if plot_style == "lines":
-            # ax.plot(line[:, 0], line[:, 1], linewidth=d_sep*200)
-            if outline:
-                ax.plot(line[:, 0], line[:, 1], 
-                        color="black", 
-                        linewidth=d_sep*900, 
-                        solid_capstyle="round")
-
-                # ax.plot(line[:, 0], line[:, 1], 
-                #         color=color, 
-                #         linewidth=d_sep*800, 
-                #         solid_capstyle="round",
-                #         path_effects=[pe.Stroke(linewidth=10, foreground='black'), pe.Normal()])
-
-            step = int(2 * d_sep / step_size) + 1
             step = 1
             line_len = line.shape[0]
-
             main_line_end = int(line_len * (np.random.rand(1)*0.3 + 0.65))
-
             ids = range(main_line_end, line.shape[0], step)
 
             for id in reversed(ids):
                 length = int(np.random.uniform(1, 10, 1))
-                ax.plot(line[id:id+length, 0], line[id:id+length, 1], 
-                        color=np.random.choice(accent, 1)[0],
-                        linewidth=d_sep*800, 
-                        solid_capstyle="round")
+                line_plot = Line(line[id:id+length, 0], line[id:id+length, 1],
+                                 color=np.random.choice(accent, 1)[0],
+                                 linewidth=width, 
+                                 solid_capstyle="round")
+                ax.add_line(line_plot)
 
-            ax.plot(line[:main_line_end, 0], line[:main_line_end, 1], 
-                    color=color, 
-                    linewidth=d_sep*800, 
-                    solid_capstyle="round")
+            line_plot = Line(line[:main_line_end, 0], line[:main_line_end, 1],
+                             linewidth=width,
+                             color=color,
+                             solid_capstyle="round")
+            ax.add_line(line_plot)
 
         if plot_style == "dots":
-            step = int(2 * d_sep / step_size) + 1
+            step = int( width / step_size) + 1
             for point in line[::step]:
-                circle = plt.Circle(tuple(point), d_sep * 0.9, color='black')
+                circle = plt.Circle(tuple(point), width / 2, color=color)
                 ax.add_patch(circle)
 
-
     plt.show()
-    ax.axis('off')
-    fig.savefig("flow-field_02.jpg", bbox_inches='tight', pad_inches = 0)
-    # fig.savefig("flow-field_02.svg", bbox_inches='tight', pad_inches = 0)
+    fig.savefig("flow-field_02.jpg", dpi=150)
